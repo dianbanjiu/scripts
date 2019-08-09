0693e7f99c09b2b65514d611f75941fafe258f3d
dianbanjiu <dianbanjiu@gmail.com>
2019-08-09 22:42:50 +0800

    update docker install script

--- README.md
+++ README.md
@@ -8,6 +8,8 @@
 - 使用 vim-plug 安装以下几个 vim 插件（插件的具体内容以及一些 vim 的简单配置位于我的另一个库 dianbanjiu/.vimrc 中，[点击此处到达](https://github.com/dianbanjiu/.vimrc)）
     - vim-airline
 
-## docker.sh
-根据 docker 官方文档的安装步骤总结的安装脚本。
-
+## docker.sh  
+建议在执行该脚本时使用普通用户，而非超级用户。  
+- 根据 docker 官方文档的安装步骤总结的安装脚本。  
+- Docker CE 及 Docker Registry 更改为中科大的镜像源（前者是用来下载 docker，后者用来获取镜像的），加速国内下载的速度。  
+- 将 docker 添加至当前用户组，之后以该用户身份使用 docker 时，可以不用加 sudo  
--- docker.sh
+++ docker.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# Install dokcer-ce for ubuntu 16.04+
-# Run it with non-root user
+# 适用于 Ubuntu 16.04 及以上的版本
+# 最好以普通用户的身份来执行该脚本
 sudo apt update
-# Preinstall essential softwares
+# 需要预先安装的软件
 sudo apt install apt-transport-https curl gnupg-agent software-properties-common
 
-# add docker office stable repo to ubuntu repo
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# 添加 docker 的官方源到 Ubuntu 的库中
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 sudo apt update
 sudo apt install docker-ce
 
-# Add docker to current user group
+# 添加 docker 到当前用户组，方便在之后可以通过普通用户的身份来使用 docker
 sudo usermod -aG docker $USER
 
+# 更改 docker 的源为中科大镜像源，加速国内的镜像获取速度
+sudo touch /etc/docker/daemon.json
+sudo echo -e "{\n    \"registry-mirrors\": [\"https://docker.mirrors.ustc.edu.cn/\"]\n}" > /etc/docker/daemon.json
